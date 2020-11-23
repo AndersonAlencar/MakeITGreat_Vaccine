37ca4efb1ee56650ee0485cb80aa45d812dc28f7
Anderson Alencar <andersonalencar@MacBook-Air-de-Anderson.local>
2020-11-23 11:58:49 -0300

    initial configurations

---
+++ Vaccine/Vaccine/Controller/MyCardViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MyCardViewController.swift
+//  Vaccine
+//
+//  Created by Anderson Alencar on 23/11/20.
+//
+
+import UIKit
+
+class MyCardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ Vaccine/Vaccine/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Vaccine
+//
+//  Created by Anderson Alencar on 23/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
